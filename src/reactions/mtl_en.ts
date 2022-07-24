4d2773e639ff703e60c39f915cba64b988678e08
Philip Duong <tangphilip3@gmail.com>
2022-07-24 12:29:45 +0200

    changed from yandex to deepl mtl

--- src/reactions/mtl_en.ts
+++ src/reactions/mtl_en.ts
@@ -1,33 +1,34 @@
 import axios from "axios";
 import { MessageReaction, PartialMessageReaction, PartialUser, User } from "discord.js";
-import FormData from "form-data";
-import { escape } from "querystring";
 import { URLSearchParams } from "url";
 import { Listener } from "./template";
 
 export const data: Listener = {
     triggerEmoji: '🇺🇸',
     execute: async (reaction: MessageReaction | PartialMessageReaction, user: User | PartialUser) => {
+        if (process.env.DEEPLAUTHKEY == undefined) {
+            await reaction.message.channel.send('No DeepL Auth Key provided! MTL failed!');
+            return;
+        }
         const text: string = reaction.message.content ?? '';
-        const autoDetectURL: string = `https://translate.yandex.net/api/v1/tr.json/detect?sid=011f69ca.62da9350.9a02e657.74722d74657874&srv=tr-text&text=${encodeURI(text)}&options=1&yu=4678998311658491538`;
-        axios.get(autoDetectURL).then(async (res) => {
+        let form: URLSearchParams = new URLSearchParams();
+        form.append('auth_key', process.env.DEEPLAUTHKEY);
+        form.append('text', text);
+        form.append('target_lang', 'EN');
+        const mtlURL: string = `https://api-free.deepl.com/v2/translate?auth_key=${process.env.DEEPLAUTHKEY}`;
+        axios.post(mtlURL, form).then(async (res) => {
             if (res.status == 200) {
-                const lang: string = res.data.lang;
-                const mtlURL: string = `https://translate.yandex.net/api/v1/tr.json/translate?id=011f69ca.62da9350.9a02e657.74722d74657874-1-0&srv=tr-text&lang=${lang}-en&reason=auto&format=text&ajax=1&yu=4678998311658491538`;
-                let form: URLSearchParams = new URLSearchParams();
-                form.append('text', text);
-                form.append('options', '4');
-                axios.post(mtlURL, form).then(async (res) => {
-                    if (res.status == 200) {
-                        const result: string = res.data.text[0];
-                        await reaction.message.channel.send(result);
-                    } else {
-                        await reaction.message.channel.send('MTL process failed!');
-                    }
-                }).catch(console.log);
+                const sourceDetected: string = res.data.translations[0].detected_source_language;
+                const result: string = res.data.translations[0].text;
+                console.log("MTL SourceLang: " + sourceDetected);
+                console.log("MTL Result: \n" + text);
+                await reaction.message.channel.send(result);
             } else {
-                await reaction.message.channel.send('Auto detection failed!');
+                await reaction.message.channel.send('DeepL MTL failed!');
             }
-        }).catch(console.log);
+        }).catch(async err => {
+            console.error(err);
+            await reaction.message.channel.send('MTL request failed!');
+        });
     }
 };
