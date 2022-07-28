402ec5ba774a680894efaeb1b1ef79664085dad1
Philip Duong <tangphilip3@gmail.com>
2022-07-28 22:58:07 +0200

    Spanish added

---
+++ src/reactions/mtl_es.ts
@@ -0,0 +1,11 @@
+
+import { MessageReaction, PartialMessageReaction, PartialUser, User } from "discord.js";
+import { MongoClient } from "mongodb";
+import { Listener, mtl } from "./template";
+
+export const data: Listener = {
+    triggerEmoji: '🇪🇸',
+    execute: async (reaction: MessageReaction | PartialMessageReaction, user: User | PartialUser, client: MongoClient) => {
+        mtl(reaction, user, client, 'ES');
+    }
+};
