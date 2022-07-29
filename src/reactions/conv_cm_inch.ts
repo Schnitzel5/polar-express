8bf516552db52a4d320c8c1577a76d5db3f36571
Philip Duong <tangphilip3@gmail.com>
2022-07-29 15:41:17 +0200

    .

--- src/reactions/conv_cm_inch.ts
+++ src/reactions/conv_cm_inch.ts
@@ -8,7 +8,7 @@ export const data: Listener = {
     triggerEmoji: '🍆',
     execute: async (reaction: MessageReaction | PartialMessageReaction, user: User | PartialUser, client: MongoClient) => {
         const num: number = parseInt(reaction.message.cleanContent?.replace(IS_NOT_NUMBER, '') ?? '0');
-        let result: number = num % 2.54;
+        let result: number = Math.round(num / 2.54 * 100) / 100;
         reaction.message.channel.send(`Whoa! A ${result}incher!`);
     }
 };
