957a119ddd7ca0f0181e834f6ecdf77f42f96f48
Philip Duong <tangphilip3@gmail.com>
2022-07-29 16:10:33 +0200

    .

--- src/reactions/mtl_hebrew.ts
+++ src/reactions/mtl_hebrew.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { MessageReaction, PartialMessageReaction, PartialUser, User } from "discord.js";
+import FormData from "form-data";
 import { MongoClient } from "mongodb";
 import { Listener } from "./template";
 
@@ -7,19 +8,19 @@ export const data: Listener = {
     triggerEmoji: '🇮🇱',
     execute: async (reaction: MessageReaction | PartialMessageReaction, user: User | PartialUser, client: MongoClient) => {
         const text: string = reaction.message.cleanContent ?? '';
-        let preForm: URLSearchParams = new URLSearchParams();
+        let preForm: FormData = new FormData();
         preForm.append('text_to_translate', text);
         const autoDetectURL: string = `https://www.translate.com/translator/ajax_lang_auto_detect`;
-        axios.post(autoDetectURL, preForm).then(async (res) => {
+        axios.post(autoDetectURL, preForm, { headers: preForm.getHeaders() }).then(async (res) => {
             if (res.status == 200) {
                 const lang: string = res.data.language;
                 const mtlURL: string = `https://www.translate.com/translator/translate_mt`;
-                let form: URLSearchParams = new URLSearchParams();
+                let form: FormData = new FormData();
                 form.append('text_to_translate', text);
                 form.append('source_lang', lang);
                 form.append('translated_lang', 'iw');
                 form.append('use_cache_only', 'false');
-                axios.post(mtlURL, form).then(async (res) => {
+                axios.post(mtlURL, form, { headers: form.getHeaders() }).then(async (res) => {
                     if (res.status == 200) {
                         const result: string = res.data.translated_text;
                         await reaction.message.channel.send(result);
